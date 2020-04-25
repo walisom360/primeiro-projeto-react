951c60b86e87efa8027701f5897841ddb2d55076
walison matheus <walisom360@hotmail.com>
2020-04-25 01:47:19 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>ola</h1>
-      </div>
-    );
-}
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
